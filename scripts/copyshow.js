b68f825de152aba908981b6b170d976a42be1069
MichaelHowe11 <michael.howell23@student.cart.org>
2024-07-01 00:57:45 -0700

    update

---
+++ scripts/copyshow.js
@@ -0,0 +1,25 @@
+let isFading = false;
+
+function fadein() {
+    if (isFading) return; 
+
+    isFading = true;
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function waitfade() {
+        let copyPopup = document.getElementById("copy");
+        copyPopup.style.display = "flex";
+        copyPopup.offsetHeight;
+        copyPopup.style.opacity = "1";
+        await sleep(4000);
+        copyPopup.style.opacity = "0";
+        await sleep(400);
+        copyPopup.style.display = "none";
+        isFading = false;
+    }
+
+    waitfade();
+}
